91bca3cd75efbd1bafa38f29518d3cb5d0ffafdb
Rafael Almeida <rafael13almeida@gmail.com>
2022-06-29 17:28:32 -0300

    Iniciando processamento com db

---
+++ README.md
@@ -0,0 +1,5 @@
+# Agenda de Contatos
+
+### Estilos
+
+Bootstrap e Font-Awesome. <https://cdnjs.com/>
---
+++ config/processo.php
@@ -0,0 +1,13 @@
+<?php
+
+session_start();
+
+include_once("conexao.php");
+include_once("url.php");
+
+$query = "SELECT * FROM contatos";
+
+$stmt = $conn->prepare($query);
+$stmt->execute();
+
+$contatos = $stmt->fetchAll();
